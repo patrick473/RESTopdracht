921845aacb3af104cb293066b5dbd543db9e40c7
patrick kottman <patkottman@gmail.com>
2018-03-13 15:52:33 +0100

    added select daos

--- src/main/java/Persistency/ClassDAO.java
+++ src/main/java/Persistency/ClassDAO.java
@@ -1,4 +1,41 @@
 package Persistency;
 
+import Domain.ClassObject;
+import Domain.Student;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class ClassDAO extends BaseDAO{
+    private ArrayList<ClassObject> getClasses(String query) {
+        ArrayList<ClassObject> results = new ArrayList<ClassObject>();
+        try (Connection con = super.getlocalConnection()) {
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            while(rs.next()){
+
+                String name = rs.getString("name");
+                int id = rs.getInt("id");
+
+
+                ClassObject classObject = new ClassObject(name,id);
+
+                results.add(classObject);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return results;
+    }
+
+    private ArrayList<ClassObject> getAllStudents(){
+        return getClasses("select * from class");
+    }
+
+    private ClassObject getClass(int id){
+        return getClasses("select * from class where id = "+id).get(0);
+    }
 }
--- src/main/java/Persistency/StudentDAO.java
+++ src/main/java/Persistency/StudentDAO.java
@@ -1,4 +1,44 @@
 package Persistency;
 
+import Domain.Student;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class StudentDAO extends BaseDAO {
+
+        private ArrayList<Student> getStudents(String query) {
+            ArrayList<Student> results = new ArrayList<Student>();
+            try (Connection con = super.getlocalConnection()) {
+                Statement stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery(query);
+
+                while(rs.next()){
+
+                    String name = rs.getString("name");
+                    int id = rs.getInt("studentid");
+                    int classid = rs.getInt("classid");
+
+                    Student student = new Student(name,id,classid);
+
+                    results.add(student);
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            return results;
+        }
+
+        private ArrayList<Student> getAllStudents(){
+            return getStudents("select * from student");
+        }
+        private ArrayList<Student> getStudentByClass(int classid){
+            return getStudents("select * from student where classid = "+classid);
+        }
+        private Student getStudent(int id){
+            return getStudents("select * from student where studentid = "+id).get(0);
+        }
+
 }
